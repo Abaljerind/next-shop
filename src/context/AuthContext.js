d4bc4fccd1c6e11f67b6ca3df997594c6d40dadc
Abaljerind <abaljerinda@gmail.com>
2026-02-05 06:53:03 +0700

    Feat(AuthContext): create new context for simple login
    Create context and context provider
    Import useState and useEffect
    Create functions: login and logout

---
+++ src/context/AuthContext.js
@@ -0,0 +1,49 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) setUser(JSON.parse(savedUser));
+  }, []);
+
+  const login = async (username, password) => {
+    const res = await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await res.json();
+
+    if (data.token) {
+      const fakeUser = {
+        fullName: "David Morrison",
+        email: "morrison@gmail.com",
+        phone: "1-570-236-7033",
+      };
+
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(fakeUser));
+      setUser(fakeUser);
+    }
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
