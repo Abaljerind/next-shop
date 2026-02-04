05201f7cd31beebf174d7c96dcfa0b6721cf66c1
Abaljerind <abaljerinda@gmail.com>
2026-02-04 15:04:31 +0700

    Feat(app/page): fill homepage with product cards and category filter
    Create 2 useState for save products data from API and save active category
    Add useEffect to fetching data using try catch finally
    Create variable productByCategory with useMemo to process products from API to display data based on it's category
    Create variable handleCategory to change value of state selectCategory
    Create variable categoriesProducts with useMemo to get all categories from data products and add category "All"
    Create 2 <section> for list of category and list of products

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,123 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useMemo, useState } from "react";
+
+import { TbCategory } from "react-icons/tb";
+
+export default function Homepage() {
+  const [products, setProducts] = useState([]);
+  const [selectCategory, setSelectCategory] = useState("All");
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+
+        if (!res.ok) {
+          throw new Error(`Failed to fetch products: ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        console.log(`Fetch error: ${err.message}`);
+      } finally {
+        console.log("Fetching data finish.");
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  // for filter data based on category
+  const productByCategory = useMemo(() => {
+    if (selectCategory === "All") return products;
+
+    return products.filter((product) => product.category === selectCategory);
+  }, [selectCategory, products]);
+
+  // for handling category active
+  const handleCategory = (category) => {
+    setSelectCategory(category);
+  };
+
+  // get all category from products
+  const categoriesProducts = useMemo(() => {
+    if (products.length === 0) return [];
+
+    return [
+      "All",
+      ...new Set(
+        products.map((product) => {
+          return product.category;
+        }),
+      ),
+    ];
+  }, [products]);
+
+  // copy and add new category "All"
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">Hello World</p>
+    <main className="flex flex-col items-center gap-4 py-4">
+      {/* list category */}
+      <section className="w-full space-y-4 rounded-xl border border-white/15 py-4">
+        <p className="flex items-center gap-2 pl-2 text-lg font-semibold tracking-wider">
+          <TbCategory className="size-6 text-purple-500" /> Categories:
+        </p>
+        <div className="grid gap-4">
+          {categoriesProducts.map((category) => {
+            const bigCategory =
+              category.charAt(0).toUpperCase() + category.slice(1);
+            return (
+              <ul
+                className={`${selectCategory === category ? "border-l-2 border-purple-500 pl-1" : ""} cursor-pointer bg-linear-to-r from-white/5 to-[#0a0a0a] py-2 pl-2 tracking-wider`}
+                key={category}
+              >
+                <li className="" onClick={() => handleCategory(category)}>
+                  {bigCategory}
+                </li>
+              </ul>
+            );
+          })}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={16} height={16} />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </section>
+      {/* ./ list category */}
+
+      {/* product list */}
+      <section className="grid items-center gap-4 p-4 md:grid-cols-3 lg:gap-6 xl:grid-cols-4">
+        {productByCategory.map((product) => {
+          return (
+            <div
+              className="flex flex-col justify-between rounded-xl border border-white/15 p-4 font-medium text-black"
+              key={product.id}
+            >
+              <img
+                src={product.image}
+                alt={product.category}
+                className="mx-auto h-72 min-h-52 w-full self-stretch rounded-lg bg-gray-500 object-cover object-top"
+              />
+              <div className="mt-4 flex flex-col gap-4">
+                <h3 className="line-clamp-1 font-medium text-white">
+                  {product.title}
+                </h3>
+                <span className="w-fit rounded-full border border-white/15 bg-[#1a1a1a] px-4 py-2 text-xs font-semibold text-white">
+                  {product.category}
+                </span>
+                <div className="mt-4 flex items-center justify-between">
+                  <div className="">
+                    <span className="text-xs text-gray-400">Price</span>
+                    <p className="text-white">${product.price}</p>
+                  </div>
+                  <button className="cursor-pointer rounded-lg bg-purple-700 px-6 py-1.5 font-medium tracking-wider text-white">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+      {/* ./ product list */}
+    </main>
   );
 }
