7326c91a770e6ded3d63538cb4ead0e423173788
Abaljerind <abaljerinda@gmail.com>
2026-02-05 03:04:27 +0700

    Feat(CartContext): create CartContext for get and send data in local storage
    Create context and save it in variable CartContext
    Create function CartProvider to be the component
    Create cart state to store data
    Use useEffect to save and take data from local storage
    Create function addToCart, removeFromCart, increaseQty, decreaseQty and variable totalPrice

---
+++ src/context/CartContext.js
@@ -0,0 +1,83 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  // ambil data dari local storage
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) setCart(JSON.parse(savedCart));
+  }, []);
+
+  // simpan data ke local storage
+  useEffect(() => {
+    if (cart.length > 0) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+
+  const addToCart = (product) => {
+    setCart((prevState) => {
+      const existingProduct = prevState.find((item) => item.id === product.id);
+
+      if (existingProduct) {
+        return prevState.map((item) => {
+          return item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        });
+      }
+
+      return [...prevState, { ...product, quantity: 1 }];
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevState) => prevState.filter((item) => item.id !== id));
+  };
+
+  const increaseQty = (id) => {
+    setCart((prevState) => {
+      return prevState.map((item) => {
+        return item.id === id ? { ...item, quantity: item.quantity + 1 } : item;
+      });
+    });
+  };
+
+  const decreaseQty = (id) => {
+    setCart((prevState) => {
+      return prevState
+        .map((item) => {
+          return item.id === id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item;
+        })
+        .filter((item) => item.quantity > 0);
+    });
+  };
+
+  const totalPrice = cart.reduce((sum, item) => {
+    return sum + item.price * item.quantity;
+  }, 0);
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        increaseQty,
+        decreaseQty,
+        totalPrice,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export const useCart = () => useContext(CartContext);
