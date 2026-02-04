53f6bd114948c37edd9b5d028ef2634ed850a710
Abaljerind <abaljerinda@gmail.com>
2026-02-04 22:29:17 +0700

    Feat(app/page): add loading and error state
    Create useState for loading and error
    Add setLoading and setError in function fetchProducts
    Add conditional rendering with state loading and error before display the products

--- src/app/page.js
+++ src/app/page.js
@@ -7,22 +7,30 @@ import { TbCategory } from "react-icons/tb";
 export default function Homepage() {
   const [products, setProducts] = useState([]);
   const [selectCategory, setSelectCategory] = useState("All");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     async function fetchProducts() {
+      setLoading(true);
+      setError("");
+
       try {
         const res = await fetch("https://fakestoreapi.com/products");
 
         if (!res.ok) {
-          throw new Error(`Failed to fetch products: ${res.statusText}`);
+          throw new Error(
+            `Failed to fetch products, please try again later. ${res.statusText}`,
+          );
         }
 
         const data = await res.json();
         setProducts(data);
       } catch (err) {
-        console.log(`Fetch error: ${err.message}`);
+        setError(`Fetch error: ${err.message}`);
       } finally {
         console.log("Fetching data finish.");
+        setLoading(false);
       }
     }
 
@@ -58,71 +66,89 @@ export default function Homepage() {
   // copy and add new category "All"
 
   return (
-    <main className="flex w-full max-w-7xl flex-col items-center gap-4 py-4 lg:flex-row-reverse lg:items-start lg:gap-6">
-      {/* list category */}
-      <section className="w-full space-y-4 rounded-xl border border-white/15 py-4 md:w-11/12 lg:w-96">
-        <p className="flex items-center gap-2 pl-2 text-lg font-semibold tracking-wider">
-          <TbCategory className="size-6 text-purple-500" /> Categories:
+    <>
+      {loading && (
+        <p className="text-purple mt-10 animate-pulse text-center text-4xl">
+          Loading...
         </p>
-        <div className="grid gap-4">
-          {categoriesProducts.map((category) => {
-            const bigCategory =
-              category.charAt(0).toUpperCase() + category.slice(1);
-            return (
-              <ul
-                className={`${selectCategory === category ? "border-l-2 border-purple-500 pl-1" : ""} cursor-pointer bg-linear-to-r from-white/5 to-[#0a0a0a] py-2 pl-2 tracking-wider`}
-                key={category}
-              >
-                <li className="" onClick={() => handleCategory(category)}>
-                  {bigCategory}
-                </li>
-              </ul>
-            );
-          })}
-        </div>
-      </section>
-      {/* ./ list category */}
+      )}
 
-      {/* product list */}
-      <section className="grid w-full items-stretch gap-4 p-4 md:grid-cols-2 md:gap-6 lg:p-0 xl:grid-cols-3">
-        {productByCategory.map((product) => {
-          const bigCategory =
-            product.category.charAt(0).toUpperCase() +
-            product.category.slice(1);
-          return (
-            <div
-              className="flex h-full flex-col justify-between rounded-xl border border-white/15 p-4 font-medium text-black"
-              key={product.id}
-            >
-              <img
-                src={product.image}
-                alt={product.category}
-                className="mx-auto aspect-[3/4] w-full rounded-lg bg-gray-500 object-cover object-top"
-              />
-              <div className="mt-4 flex flex-col gap-4">
-                <h3 className="line-clamp-1 font-medium text-white lg:text-lg">
-                  {product.title}
-                </h3>
-                <span className="w-fit rounded-full border border-white/15 bg-[#1a1a1a] px-4 py-2 text-xs font-semibold tracking-wide text-white">
-                  {bigCategory}
-                </span>
-                <div className="mt-4 flex items-center justify-between">
-                  <div className="">
-                    <span className="text-xs text-gray-400 lg:text-sm">
-                      Price
-                    </span>
-                    <p className="text-white lg:text-lg">${product.price}</p>
-                  </div>
-                  <button className="cursor-pointer rounded-lg bg-purple-700 px-6 py-1.5 font-medium tracking-wider text-white">
-                    Add to cart
-                  </button>
-                </div>
-              </div>
+      {error && !loading && (
+        <p className="mt-20 mb-4 text-center text-4xl text-red-500">{error}</p>
+      )}
+
+      {!loading && !error && (
+        <main
+          className={`${loading ? "hidden" : "flex"} w-full max-w-7xl flex-col items-center gap-4 py-4 lg:flex-row-reverse lg:items-start lg:gap-6`}
+        >
+          {/* list category */}
+          <section className="w-full space-y-4 rounded-xl border border-white/15 py-4 md:w-11/12 lg:w-96">
+            <p className="flex items-center gap-2 pl-2 text-lg font-semibold tracking-wider">
+              <TbCategory className="size-6 text-purple-500" /> Categories:
+            </p>
+            <div className="grid gap-4">
+              {categoriesProducts.map((category) => {
+                const bigCategory =
+                  category.charAt(0).toUpperCase() + category.slice(1);
+                return (
+                  <ul
+                    className={`${selectCategory === category ? "border-l-2 border-purple-500 pl-1" : ""} cursor-pointer bg-linear-to-r from-white/5 to-[#0a0a0a] py-2 pl-2 tracking-wider`}
+                    key={category}
+                  >
+                    <li className="" onClick={() => handleCategory(category)}>
+                      {bigCategory}
+                    </li>
+                  </ul>
+                );
+              })}
             </div>
-          );
-        })}
-      </section>
-      {/* ./ product list */}
-    </main>
+          </section>
+          {/* ./ list category */}
+
+          {/* product list */}
+          <section className="grid w-full items-stretch gap-4 p-4 md:grid-cols-2 md:gap-6 lg:p-0 xl:grid-cols-3">
+            {productByCategory.map((product) => {
+              const bigCategory =
+                product.category.charAt(0).toUpperCase() +
+                product.category.slice(1);
+              return (
+                <div
+                  className="flex h-full flex-col justify-between rounded-xl border border-white/15 p-4 font-medium text-black"
+                  key={product.id}
+                >
+                  <img
+                    src={product.image}
+                    alt={product.category}
+                    className="mx-auto aspect-[3/4] w-full rounded-lg bg-gray-500 object-cover object-top"
+                  />
+                  <div className="mt-4 flex flex-col gap-4">
+                    <h3 className="line-clamp-1 font-medium text-white lg:text-lg">
+                      {product.title}
+                    </h3>
+                    <span className="w-fit rounded-full border border-white/15 bg-[#1a1a1a] px-4 py-2 text-xs font-semibold tracking-wide text-white">
+                      {bigCategory}
+                    </span>
+                    <div className="mt-4 flex items-center justify-between">
+                      <div className="">
+                        <span className="text-xs text-gray-400 lg:text-sm">
+                          Price
+                        </span>
+                        <p className="text-white lg:text-lg">
+                          ${product.price}
+                        </p>
+                      </div>
+                      <button className="cursor-pointer rounded-lg bg-purple-700 px-6 py-1.5 font-medium tracking-wider text-white">
+                        Add to cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          {/* ./ product list */}
+        </main>
+      )}
+    </>
   );
 }
