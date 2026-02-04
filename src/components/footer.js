a1082ef3a4627312a541824d03c7164b66dec279
Abaljerind <abaljerinda@gmail.com>
2026-02-04 09:45:03 +0700

    Feat(Footer): create simple footer

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="">
+      made with ❤️ by{" "}
+      <a href="https://github.com/Abaljerind" target="_blank" className="text-red-400">
+        AbalJerind
+      </a>
+    </footer>
+  );
+};
